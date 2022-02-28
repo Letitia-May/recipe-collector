823f75efa5fe2d6582250d81d82d286eedbc2390
Chloe <chloe.kuypers@99designs.com>
2022-02-28 18:06:59 +1100

    Add mysql db connection

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+// Temporary global variable
+var db *sql.DB
 
 func main() {
-	fmt.Println("G'day!")
+	// Capture connection properties
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "recipe_collector",
+	}
+	// Get a db handle
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Confirms the db connection
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected")
 }
