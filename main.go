e78a758bf11ef0ab5aa232076399b904d629404a
Chloe <chloe.kuypers@99designs.com>
2022-03-28 15:51:51 +1100

    Query from recipes table

--- main.go
+++ main.go
@@ -12,6 +12,18 @@ import (
 // Temporary global variable
 var db *sql.DB
 
+type Recipe struct {
+	ID          int64
+	Title       string
+	Description *string
+	Time        *string
+	Servings    *string
+	Url         *string
+	Notes       *string
+	Rating      *float32
+	TimesCooked *int64
+}
+
 func main() {
 	// Capture connection properties
 	cfg := mysql.Config{
@@ -21,6 +33,7 @@ func main() {
 		Addr:   os.Getenv("DBADDR"),
 		DBName: "recipe_collector",
 	}
+
 	// Get a db handle
 	var err error
 	db, err = sql.Open("mysql", cfg.FormatDSN())
@@ -34,4 +47,72 @@ func main() {
 		log.Fatal(pingErr)
 	}
 	fmt.Println("Connected")
+
+	recipes, err := allRecipes()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Recipes found: ")
+	for _, r := range recipes {
+		fmt.Println(r)
+	}
+}
+
+func (r Recipe) String() string {
+	s := fmt.Sprintf("%d %s", r.ID, r.Title)
+
+	if r.Description != nil {
+		s = s + fmt.Sprintf(" %s", *r.Description)
+	}
+
+	if r.Time != nil {
+		s = s + fmt.Sprintf(" %s", *r.Time)
+	}
+
+	if r.Servings != nil {
+		s = s + fmt.Sprintf(" %s", *r.Servings)
+	}
+
+	if r.Url != nil {
+		s = s + fmt.Sprintf(" %s", *r.Url)
+	}
+
+	if r.Notes != nil {
+		s = s + fmt.Sprintf(" %s", *r.Notes)
+	}
+
+	if r.Rating != nil {
+		s = s + fmt.Sprintf(" %f", *r.Rating)
+	}
+
+	if r.TimesCooked != nil {
+		s = s + fmt.Sprintf(" %d", *r.TimesCooked)
+	}
+
+	return s
+}
+
+func allRecipes() ([]Recipe, error) {
+	var recipes []Recipe
+
+	rows, err := db.Query("SELECT * FROM recipes")
+	if err != nil {
+		return nil, fmt.Errorf("allRecipes: %v", err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var recipe Recipe
+		if err := rows.Scan(&recipe.ID, &recipe.Title, &recipe.Description, &recipe.Time, &recipe.Servings, &recipe.Url, &recipe.Notes, &recipe.Rating, &recipe.TimesCooked); err != nil {
+			return nil, fmt.Errorf("allRecipes: %v", err)
+		}
+		recipes = append(recipes, recipe)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("allRecipes: %v", err)
+	}
+
+	return recipes, nil
 }
