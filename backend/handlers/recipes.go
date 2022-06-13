d3254c97dfdae4717b340ca667f3ed910b38c0ac
Chloe <chloe.kuypers@99designs.com>
2022-06-13 22:21:16 +1000

    Get ingredients for each section

--- backend/handlers/recipes.go
+++ backend/handlers/recipes.go
@@ -31,9 +31,9 @@ type step struct {
 }
 
 type ingredientSection struct {
-	ID      int64  `json:"-"`
-	Heading string `json:"heading"`
-	// List   []string `json:"list"`
+	ID          int64    `json:"-"`
+	Heading     string   `json:"heading"`
+	Ingredients []string `json:"ingredients"`
 }
 
 type recipesResource struct {
@@ -185,5 +185,37 @@ func getRecipeIngredientSections(db *sql.DB, id int64) ([]ingredientSection, err
 		return nil, fmt.Errorf("getRecipeIngredientSections %d: %v", id, err)
 	}
 
+	for i := range ingredientSections {
+		ingredients, err := getSectionIngredients(db, ingredientSections[i].ID)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ingredientSections[i].Ingredients = ingredients
+	}
+
 	return ingredientSections, nil
+}
+
+func getSectionIngredients(db *sql.DB, id int64) ([]string, error) {
+	var ingredients []string
+
+	rows, err := db.Query("SELECT description FROM ingredients WHERE ingredient_header_id = ?", id)
+	if err != nil {
+		return nil, fmt.Errorf("getSectionIngredients %d: section has no ingredients", id)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var ingredient string
+		if err := rows.Scan(&ingredient); err != nil {
+			return nil, fmt.Errorf("getSectionIngredients %d: %v", id, err)
+		}
+		ingredients = append(ingredients, ingredient)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("getSectionIngredients %d: %v", id, err)
+	}
+
+	return ingredients, nil
 }
