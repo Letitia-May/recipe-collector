c79822ce2b78a13925f632ddfd2cee3f3661396a
Chloe <chloe.kuypers@99designs.com>
2023-06-05 15:44:01 +1000

    Search on recipe title and ingredients list

--- backend/queries/search.go
+++ backend/queries/search.go
@@ -8,7 +8,7 @@ import (
 func SearchRecipes(db *sql.DB, searchTerm string) ([]recipeSummary, error) {
 	var recipes []recipeSummary
 
-	rows, err := db.Query("SELECT id, title, description, time, servings FROM recipes WHERE recipes.title LIKE ?", "%"+searchTerm+"%")
+	rows, err := db.Query("SELECT rcp.id, rcp.title, rcp.description, rcp.time, rcp.servings FROM recipes rcp JOIN ingredient_headers ih ON ih.recipe_id = rcp.id JOIN ingredients ing ON ing.ingredient_header_id = ih.id WHERE rcp.title LIKE ? OR ing.description LIKE ? GROUP BY rcp.id", "%"+searchTerm+"%", "%"+searchTerm+"%")
 	if err != nil {
 		return nil, fmt.Errorf("searchRecipes: %v", err)
 	}
